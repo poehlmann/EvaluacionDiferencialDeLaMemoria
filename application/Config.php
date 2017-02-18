817cfcdba0acadc2db51ed1d9a6c7af0d1e76986
poehlmann <bruno.poehlmann@hotmail.com>
2017-02-18 17:17:39 -0400

    configuracion para el server hostinger: pagina web : neurolab.hol.es

--- application/Config.php
+++ application/Config.php
@@ -1,18 +1,18 @@
 <?php
 
+define('BASE_URL', 'https://neurolap.hol.es');
 
-define('BASE_URL', 'http://localhost:8080/mvc/');
 define('DEFAULT_CONTROLLER', 'index');
-define('DEFAULT_LAYOUT', 'twb');
+define('DEFAULT_LAYOUT','twb');
 
-define('APP_NAME', 'TESIS');
+define('APP_NAME', 'BPM');
 define('APP_SLOGAN', 'NEUROLAP');
-define('APP_COMPANY', 'UPSA-TESIS');
+define('APP_COMPANY', 'UPSA-BPM');
 define('SESSION_TIME', 10);
 define('HASH_KEY', '4f6a6d832be79');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'tesisfinal');
+define('DB_HOST', '	mysql.hostinger.co');
+define('DB_USER', 'u473891975_bpm');
+define('DB_PASS', 'poehlmann0110F');
+define('DB_NAME', 'u473891975_neuro');
 define('DB_CHAR', 'utf8');
