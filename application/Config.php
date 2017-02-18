6c803563dab30c3c3084447c44c88eac77e23c3c
poehlmann <bruno.poehlmann@hotmail.com>
2017-02-18 17:30:13 -0400

    configuracion para el server hostinger: pagina web : neurolab.hol.es

--- application/Config.php
+++ application/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL', 'https://neurolap.hol.es');
+define('BASE_URL', 'http://www.neurolab.hol.es');
 
 define('DEFAULT_CONTROLLER', 'index');
 define('DEFAULT_LAYOUT','twb');
@@ -11,7 +11,7 @@ define('APP_COMPANY', 'UPSA-BPM');
 define('SESSION_TIME', 10);
 define('HASH_KEY', '4f6a6d832be79');
 
-define('DB_HOST', '	mysql.hostinger.co');
+define('DB_HOST', 'mysql.hostinger.co');
 define('DB_USER', 'u473891975_bpm');
 define('DB_PASS', 'poehlmann0110F');
 define('DB_NAME', 'u473891975_neuro');
